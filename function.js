86315aad77a81f5b3ec401e2dd765de5397a4586
adithyaramesh123 <150226424+adithyaramesh123@users.noreply.github.com>
2024-05-21 14:47:56 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    let c=a+b;
+    console.log(c)
+}
+add(1,2);
+function add(fname,lname){
+    console.log(`${fname} ${lname}`);
+}
+add("Adithya","Rameshan");
---
+++ script.js
@@ -0,0 +1,102 @@
+// console.log("Hello world!!");
+// let a=1;
+// let b=2;
+// console.log(a+b);
+// let a=40;
+// let b=20;
+// console.log(a%b);
+// postfix increment
+// let a=1;
+// let b= a++;
+// console.log(a);
+// console.log(b);
+// prefix increment
+// let p=1;
+// let q= ++p;
+// console.log(p);
+// console.log(q);
+// a=10;
+// console.log(a);
+// objects
+let car={
+    fname :"swift",
+    model:"swift",
+    colour: "BLUE",
+};
+// console.log(car);
+// console.log(car.fname);
+// console.log(car.model);
+// console.log(car.colour);
+// let person={
+    // fname:"Adithya Ramesh",
+
+// address:{
+    // street :"Tly",
+    // place : "Knr",
+    // pin: 6025,
+
+// },
+// age: 20
+
+// }
+// console.log(person.address.place);
+// console.log(person);
+// console.log(person.address.street);
+// console.log(person.address.pin);
+// console.log(person.age);
+var toothbrush=new Object();
+toothbrush.name="Colgate";
+toothbrush.colour="White";
+// console.log(toothbrush);
+// console.log(toothbrush.name);
+// console.log(toothbrush.colour);
+// concatination
+let fname="Adithya";
+let lname="Rameshan";
+console.log(`${fname} ${lname}`)
+let m=5;
+let n=6;
+// console.log(` The sum is ${5}${6}`)
+// console.log(` The Sum Is ${m+n}`);
+// Array
+let Fruits = [ "Apple", " Orange","Mango",28]
+// console.log(Fruits)
+// console.log(Fruits[0]);
+// console.log(Fruits.length);
+// console.log(Fruits);
+// Fruits.push("Grapes");
+// Fruits.unshift("pear");
+// console.log(Fruits);
+// Fruits.pop();
+// console.log(Fruits);
+// Fruits.shift();
+// console.log(Fruits);
+// Array of Object
+let BSC =[
+    {
+        fname:"FEMINA",
+        age:18,
+    },
+    {
+        fname:"DEVIKA",
+        age:18,
+    },
+    {
+        fname:"ALEENA",
+        age:19,
+    },
+    {
+        fname:"kRIPA",
+        age:18,
+    },
+    {
+        fname:"ANAINA",
+        age:19,
+    },
+    {
+        fname:"ADITHYA",
+        age:19,
+    },
+];
+// console.log(BSC);
+console.log(BSC[0].age);
